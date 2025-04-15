c3192850eb62d40770d6f2748106d0bcac2f7c9e
dljardim <dljardim@outlook.com>
2025-04-15 08:41:18 -0500

    Initial Commit

---
+++ GrillMaster/GrillMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrillMasterApp.swift
+//  GrillMaster
+//
+//  Created by Damian Jardim on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct GrillMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
